6e28ca617c6e03782bfdbbc2d936ccdb2c21e30b
afroj30 <afroj.usa@gmail.com>
2023-11-20 23:48:41 -0500

    first commit

---
+++ src/main/java/com/BankOfAmerica/LoginTest.java
@@ -0,0 +1,17 @@
+package com.BankOfAmerica;
+
+public class LoginTest {
+	
+	int salary=5000;
+	String name="Afsana";
+	
+	public static void main(String[] args) {
+		int salary=5000;
+		String name="Afsana";
+		
+		System.out.println(name);
+		System.out.println(salary);
+		
+	}
+
+}
